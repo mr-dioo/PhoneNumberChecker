77880ed0046d309ff74bc747ee18be17459b4731
mr-dioo <63169446+mr-dioo@users.noreply.github.com>
2024-03-23 17:36:55 +0000

    added validation functionality and reset

--- script.js
+++ script.js
@@ -6,9 +6,30 @@ const resultsDiv = document.getElementById('results-div');
 
 
 const checkValidInput = () => {
+  reset();
   if (userInput.value === '') {
     alert("Please provide a phone number")
   }
+  else {
+    checkValidUsNumber(userInput.value)
+  }
+}
+
+const checkValidUsNumber = (input) => {
+  const usNumRegex = /^1? ?((\(\d{3}\))|(\d{3}))[- ]?\d{3}[- ]?\d{4}/;
+  showResult( usNumRegex.test(input) , input );
+}
+
+const showResult = (bool, input) => {
+  resultsDiv.textContent = (bool ? 'Valid US number: ' : 'Invalid US number: ') + input;
+  resultsDiv.classList.remove('hidden');
+  resultsDiv.classList.add(`${bool ? '' : 'in'}valid`);
+
+}
+
+const reset = () => {
+  userInput.value = '';
+  resultsDiv.className = 'hidden';
 }
 
 
